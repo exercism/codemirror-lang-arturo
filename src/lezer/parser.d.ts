b597debfe9767d02578aadc8df5d159a62a9976f
dem4ron <demaaron88@gmail.com>
2024-07-23 11:11:03 +0200

    Create declaration file

---
+++ src/lezer/parser.d.ts
@@ -0,0 +1,12 @@
+import { LRParser } from "@lezer/lr";
+
+declare const spec_identifier: {
+  __proto__: null;
+  true: number;
+  false: number;
+  maybe: number;
+  null: number;
+  function: number;
+};
+
+export declare const parser: LRParser;
