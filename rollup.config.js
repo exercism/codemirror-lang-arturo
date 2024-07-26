bb010677085df3f2a2c7980dacda8154ed6c1b80
dem4ron <demaaron88@gmail.com>
2024-07-26 16:02:59 +0200

    Adjust Package json module and exports

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ export default {
   input: "src/index.ts",
   external: (id) => id != "tslib" && !/^(\.?\/|\w:)/.test(id),
   output: [
-    { file: "dist/index.cjs", format: "cjs" },
+    { file: "./dist/index.cjs", format: "cjs" },
     { file: "./dist/index.es.js", format: "es" },
   ],
   plugins: [lezer(), json(), typescript()],
